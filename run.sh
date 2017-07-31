713f490b61e6af38d0d43c141126868f909b400b
hummel22 <hummel.kevin@gmail.com>
2017-07-30 23:05:51 -0400

    - Created run scripts

---
+++ run.sh
@@ -0,0 +1,8 @@
+
+CURRENT=`pwd`
+cd src/main/resources/assets/
+npm install
+webpack --progress --colors
+
+cd $CURRENT
+mvn clean install && java -jar ./target/com.olledeux-0.0.1.jar server config.yml
