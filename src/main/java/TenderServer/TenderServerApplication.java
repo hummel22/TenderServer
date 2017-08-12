86bbed301b72fc666e2a500e87f9e814db7e4cec
hummel22 <hummel.kevin@gmail.com>
2017-08-12 19:19:52 -0400

    - Enable Cors in server

--- src/main/java/TenderServer/TenderServerApplication.java
+++ src/main/java/TenderServer/TenderServerApplication.java
@@ -12,6 +12,11 @@ import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TenderServerApplication extends Application<TenderServerConfiguration> {
 
@@ -64,6 +69,19 @@ public class TenderServerApplication extends Application<TenderServerConfigurati
     public void run(final TenderServerConfiguration configuration,
                     final Environment environment) {
         // TODO: implement application
+
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
 
         /**
          * Register Location API
